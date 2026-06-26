b0850f3835d0845050063a6a677d72d359606d35
Oseias Dev <oseiasbetodev@gmail.com>
2026-06-26 14:02:29 +0100

    Kua

--- src/components/drawer/Drawer.vue
+++ src/components/drawer/Drawer.vue
@@ -94,7 +94,7 @@ watch(() => props.isOpen, (newValue) => {
     if (window?.WTN) {
       window.WTN.statusBar({
         style: 'light',
-        color: '00000080',
+        color: '262626',
         overlay: false
       });
     }
