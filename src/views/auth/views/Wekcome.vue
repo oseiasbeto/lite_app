808f382686e70e6d522b849062d31ba7bcf41f39
Oseias Dev <oseiasbetodev@gmail.com>
2026-08-21 15:48:14 +0100

    ola

--- src/views/auth/views/Wekcome.vue
+++ src/views/auth/views/Wekcome.vue
@@ -1,5 +1,8 @@
 <script setup>
 const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+import {
+  login as loginGoogle
+} from "webtonative/SocialLogin/google";
 
 if (isDark) {
   window.WTN.setNavigationBarColor({ color: "#000000" });
@@ -20,6 +23,14 @@ if (isDark) {
 
   document.documentElement.classList.remove('dark');
 }
+
+const signInWithGoogle = () => {
+  loginGoogle({
+    callback: function (value) {
+      console.log(value)
+    }
+  });
+};
 </script>
 
 <template>
@@ -56,6 +67,21 @@ if (isDark) {
         <button @click="$router.push('/auth/signup')"
           class="w-full py-3 bg-x-dark-blue hover:bg-x-light-blueHover active:bg-x-light-blueActive text-white font-bold rounded-full transition">
           Criar conta
+        </button>
+
+        <button @click="signInWithGoogle"
+          class="w-full py-3 flex items-center justify-center gap-2 border text-[rgb(40,40,41)] dark:text-inherit dark:border-[rgb(57,56,57)] font-bold rounded-full transition">
+          <svg width="18" height="18" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#FFC107"
+              d="M43.611 20.083H42V20H24v8h11.303c-1.649 4.657-6.08 8-11.303 8-6.627 0-12-5.373-12-12s5.373-12 12-12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4 12.955 4 4 12.955 4 24s8.955 20 20 20 20-8.955 20-20c0-1.341-.138-2.65-.389-3.917z" />
+            <path fill="#FF3D00"
+              d="M6.306 14.691l6.571 4.819C14.655 15.108 18.961 12 24 12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4 16.318 4 9.656 8.337 6.306 14.691z" />
+            <path fill="#4CAF50"
+              d="M24 44c5.166 0 9.86-1.977 13.409-5.192l-6.19-5.238A11.91 11.91 0 0 1 24 36c-5.202 0-9.619-3.317-11.283-7.946l-6.522 5.025C9.505 39.556 16.227 44 24 44z" />
+            <path fill="#1976D2"
+              d="M43.611 20.083H42V20H24v8h11.303a12.04 12.04 0 0 1-4.087 5.571l.003-.002 6.19 5.238C36.971 39.205 44 34 44 24c0-1.341-.138-2.65-.389-3.917z" />
+          </svg>
+          Continuar com o Google
         </button>
 
         <button @click="$router.push('/auth/signin')"
