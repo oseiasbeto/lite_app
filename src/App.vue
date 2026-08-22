997f446d0cd34e83f3801223c03cc563b4797252
Oseias Dev <oseiasbetodev@gmail.com>
2026-08-22 11:59:38 +0100

    Remove clear Cache

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,6 @@ import NetworkStatusBanner from "./components/UI/NetworkStatusBanner.vue"
 import { useNetworkStatus } from "@/composables/useNetworkStatus";
 import ToastContainer from "./components/UI/ToastContainer.vue";
 import Confirmdialog from "./components/UI/Confirmdialog.vue";
-import { clearAppCache } from "webtonative";
 
 // Estado de loading do app
 const loading = ref(true)
@@ -363,8 +362,6 @@ const handleRefreshToken = async () => {
           }
         });
       }
-
-      clearAppCache(true)
     })
 }
 
